657e818b3934f97a05ca47c913b95e5de3d02293
chenpetro <chenpetro@gmail.com>
2024-10-30 18:35:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+// const printMessage = function (message) {
+//     console.log(message);
+// };
+// const higherOrderFunction = function (callback) {
+// const string = 'HOCs are awesome';    callback(string);
+// };
+// higherOrderFunction(printMessage);
+
+// const names = [
+//   "dana",
+//   "vova",
+//   "lada",
+//   "masha",
+//   "dana",
+//   "vova",
+//   "lada",
+//   "masha",
+//   "dana",
+//   "vova",
+//   "lada",
+//   "masha",
+// ];
+// const filter = function (array) {
+//   let filterNames = [];
+//   for (const element of array) {
+//     if (element.includes("d")) {
+//       filterNames.push(element);
+//     }
+//   }
+//   return console.log(filterNames);
+// };
+// filter(names);
+
+
+
+
+
+
+// const randomNumber = [10, 56, 34, 90, 23, 56, 45, 12, 34, 56];
+
+// const filterNumber = function (num) {
+//   let numberFixed = [];
+//   for (const number of num) {
+//     if (number > 50) {
+//       numberFixed.push(number);
+//     }
+//   }
+//   return console.log(numberFixed);
+// };
+// filterNumber(randomNumber);
+
+
+// // Звичайний функціональний вираз
+// const add = function(a, b, c) {
+//     return a + b + c;
+//   };
+ 
+//   // Теж саме записано, як стрілкова функція
+//   const add2 = (a, b, c) => {
+//     let g = 10
+//     return a + b + c + g;
+//   }
+// console.log(  add2(2,3,4)
+// );
+  
+
+const hello1 = () => 'hello JavaScript'
+    console.log(hello1());
+     
+
+
